079c1031b1ea22758254e5eafb393c7e516e811c
VerDmi <verdmi98@gmail.com>
2025-06-21 11:44:20 +0300

    Разработан расчёт суммы, разности, произведения и частного двух чисел, принимаемых через консоль

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        int sum = firstNumber + secondNumber;
+        int difference = firstNumber - secondNumber;
+        int product = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+
+        System.out.println(" ");
+        System.out.println("Сумма чисел: ");
+        System.out.println(firstNumber +" + " +secondNumber +" = " +sum);
+        System.out.println("-----");
+        System.out.println("Разность чисел: ");
+        System.out.println(firstNumber +" - " +secondNumber +" = " +difference);
+        System.out.println("-----");
+        System.out.println("Произведение чисел: ");
+        System.out.println(firstNumber +" * " +secondNumber +" = " +product);
+        System.out.println("-----");
+        System.out.println("Частное чисел: ");
+        System.out.println(firstNumber +" / " +secondNumber +" = " +quotient);
     }
 }
