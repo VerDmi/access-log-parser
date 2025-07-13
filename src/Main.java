4e7fb4acff8685ac4b85e2a2e2d696e8b6de2af3
VerDmi <verdmi98@gmail.com>
2025-07-13 18:29:58 +0300

    Создан бесконечный цикл, в котором в консоли будет запрашиваться путь к файлу

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count = 1;
+        while (true) {
+            System.out.print("Укажите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+
+            boolean noExists = !file.exists();
+            if (noExists) {
+                System.out.println("Указанный файл не существует");
+                continue;
+            }
+
+            boolean isDirectory = file.isDirectory();
+            if (isDirectory) {
+                System.out.println("Указанный путь является путём к папке, а не к файлу");
+                continue;
+            }
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + count);
+            count++;
+        }
     }
 }
